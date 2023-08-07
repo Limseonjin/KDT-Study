3df7391b7fb9418798656d211505501f7ec86b8a
Seonjin <128454779+Limseonjin@users.noreply.github.com>
2023-08-07 14:35:12 +0900

    ajax : Get과 Post 활용 연습

--- 3.ajax/2.서버통신/1.app.js
+++ 3.ajax/2.서버통신/1.app.js
@@ -1,45 +1,81 @@
 
 //ul 태그
 const $postUl = document.querySelector('.posts');
+//form 태그
+const $addForm = document.querySelector('#new-post form');
 
-// 서버와 통신하기 
 
-const xhr = new XMLHttpRequest;
-// console.log(xhr);
+//서버에서 게시물들을 가져와서 화면에 렌더링 함수 
+const feachGetPosts = () =>{
+  // 서버와 통신하기 
+  const xhr = new XMLHttpRequest;
+  
+  xhr.open('GET','http://localhost:5000/posts');
 
-//통신 정보를 전달 
-/*
-    HTTP method 
-    -GET: 리소스 조회(조회, 마이페이지)
-    -POST: 리소스 등록 (쓰기, 회원가입, 장바구니)
-    -PUT: 리소스 일괄수정 (게시물의 모든 정보를 통채로 수정)
-    -PATCH: 리소스 일부수정 (게시물의 제목만 수정)
-    -DELETE: 리소스를 삭제 (게시물 삭제, 회원탈퇴, 좋아요 취소)
-*/
-xhr.open('GET','http://localhost:5000/posts');
+  //요청 보내기 
+  xhr.send();
 
-//요청 보내기 
-xhr.send();
+  //응답 데이터 가져오기 
+  xhr.onload = e=> {
+      //요청이 끝난 후 xhr객체를 보면 응답 정보가 들어있습니다.
+    //console.log(xhr.response);  
 
-//응답 데이터 가져오기 
-xhr.onload = e=> {
-    //요청이 끝난 후 xhr객체를 보면 응답 정보가 들어있습니다.
-  //console.log(xhr.response);  
+    //서버에서 온 데이터는 js가 아니라 json이다. 따라서 js에서 사용하려면 json을 js로 변환해야함
+    //JSON -> JS : JSON.parse()
+    //JS -> JSON : JSON.stringify()
+    const postList = JSON.parse(xhr.response);
+    //console.log(postList);
 
-  //서버에서 온 데이터는 js가 아니라 json이다. 따라서 js에서 사용하려면 json을 js로 변환해야함
-  //JSON -> JS : JSON.parse()
-  //JS -> JSON : JSON.stringify()
-  const postList = JSON.parse(xhr.response);
-  //console.log(postList);
-
-  postList.forEach(post => {
-    const $postLi = document.createElement('li');
-    $postLi.classList.add('post-item');
-    $postLi.innerHTML=`
-        <h2>${post.title}</h2>
-        <p>${post.body}</p>
-        <button>DELETE</butoon>
-    `;
-    $postUl.appendChild($postLi);
-  });
+    postList.forEach(post => {
+      const $postLi = document.createElement('li');
+      $postLi.classList.add('post-item');
+      $postLi.innerHTML=`
+          <h2>${post.title}</h2>
+          <p>${post.body}</p>
+          <button>DELETE</butoon>
+      `;
+      $postUl.appendChild($postLi);
+    });
+  }
 };
+
+feachGetPosts();
+
+const fetchNewPost = (e) =>{
+  e.preventDefault(); //form의 새로고침 기능 중단 
+  // 서버와 통신하기 
+  const xhr = new XMLHttpRequest;
+  
+  xhr.open('POST','http://localhost:5000/posts');
+
+  const payload = JSON.stringify({
+    title : document.getElementById('title').value,
+    body : document.getElementById('content').value
+  })
+
+  xhr.setRequestHeader('content-type','application/json');
+  //요청 보내기 
+  xhr.send(payload);
+
+  //응답 상황 처리 
+  xhr.onload = e=>{
+    if (xhr.status === 200 || xhr.status === 201){
+      alert('게시물 등록 성공!');
+    }else {
+      alert('게시물 등록 실패!');
+    }
+  }
+}
+
+//폼 태그 전송 이벤트 등록 
+$addForm.addEventListener('submit',fetchNewPost);
+
+document.getElementById('go-link').addEventListener('click', e=>{
+  const flag = confirm('정말 이동하시겠습니까?');
+  if (!flag){
+    console.log('너 못갛ㅎ');
+    e.preventDefault(); //태그의 기본 기능을 없앰. 
+                        //a -> link 이동 / checkbox -> check / form -> 서버 데이터 전달 및 새로고침
+
+  }
+})
