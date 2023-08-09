93109c38b25d06dd10adb3314f2bc4a46146b832
Seonjin <128454779+Limseonjin@users.noreply.github.com>
2023-08-09 09:44:58 +0900

    [강사님] ajax 예제 : 렌더링 및 체크박스 처리

--- 3.ajax/TodoList/assets/scripts/app.js
+++ 3.ajax/TodoList/assets/scripts/app.js
@@ -1,128 +1,43 @@
-const URL = "http://localhost:5000/todos";
+const URL = 'http://localhost:5000/todos';
 
-// UL 태그
-const $todoUl = document.querySelector(".todo-list");
+const $todoList = document.querySelector('.todo-list');
 
-// form 태그
-const $addForm = document.querySelector(".todo-insert");
-
-
-//조회
-fetch(URL)
-  .then(res => res.json())
-  .then((todoList) => {
-    // 템플릿을 가져옴
-    const $listTemplate = document.getElementById("single-list");
-
-   todoList.forEach(({ id, text }) => {
-      // 템플릿태그에서 li태그를 추출
-      const $todoLi = document.importNode($listTemplate.content, true);
-      $todoLi.querySelector(".todo-list-item").dataset.id = id;
-      $todoLi.querySelector(".text").textContent = text;
-
-      $todoUl.appendChild($todoLi);
-    });
-});
-
-
-//추가
-$addForm.addEventListener("click", (e) => {
-  if (!e.target.matches("button")) return ;
-  const payload = {
-    text : document.getElementById("todo-text").value,
-    done : false
+// step1. db.json에 있는 todos를 화면에 렌더링하기
+const fetchTodos = (url, method='GET', payload=null) => {
+  const requestInit = {
+    method: method,
+    headers: { 'Content-Type': 'application/json' }
   };
-  // fetch로 POST요청 보내는법 
-  fetch(URL,{
-    method : 'POST',
-    headers : {
-      'content-type' : 'application/json',
-    },
-    body : JSON.stringify(payload)
-  })
-  .then(res => {
-    if (xhr.status === 200 || xhr.status === 201) {
-      alert("등록 성공!");
-    } else {
-      alert("등록 실패!");
-    }
-  })
-});
+  if (payload) requestInit.body = JSON.stringify(payload);
 
-const listHandler = e =>{
-    e.preventDefault();
-    // 삭제 클릭 대상 아이디 잡아오기
-    const id = e.target.closest(".todo-list-item").dataset.id;
+  return fetch(url, requestInit);
+};
 
-    //삭제 기능
-    const deleteTodo = () => {
-    fetch(`${URL}/${id}`,{
-        method : 'DELETE'
-    }).then(res => {
-        if (xhr.status === 200) {
-        alert("삭제 성공!");
-        } else {
-        alert("삭제 실패!");
-        }
+// 화면에 todos를 렌더링하는 함수
+const renderTodos = (todoList) => {
+  // li태그의 템플릿을 가져옴
+  const $liTemplate = document.getElementById('single-todo');
+
+  todoList.forEach(({ id, text, done }) => {
+    // console.log('todo: ', todo);
+    const $newLi = document.importNode($liTemplate.content, true);
+    $newLi.querySelector('li').dataset.id = id; // data-id 속성 부여
+    $newLi.querySelector('.text').textContent = text;
+    const $checkbox = $newLi.querySelector('.checkbox input[type=checkbox]');
+    $checkbox.checked = done;
+
+    done && $checkbox.parentNode.classList.add('checked')
+    $todoList.appendChild($newLi);
+  });
+};
+
+// =========== 앱 실행 =========== //
+const init = () => {
+  fetchTodos(URL)
+    .then(res => res.json())
+    .then(todos => {
+      renderTodos(todos);
     });
-    };
-    
-    //체크 박스 기능
-    const checkTodo = () =>{
-        let isCheck = {done:false};
-    
-        if (e.target.checked){
-            isCheck = {done : true};
-        }
-        
-        fetch(`${URL}/${id}`,{
-            method : 'PATCH',
-            headers : {
-                'content-type' : 'application/json',
-            },
-            body : JSON.stringify(isCheck)
-        })
-        .then(res => {
-            if (!res.ok){
-                console.log("에러에러");
-            }
-            return res.json();
-        })
-            
-    }
-    const insertTodo = ()=>{
-        const $text = $todoUl.querySelector('.text');
+};
 
-        console.log($text);
-        $text.innerHTML = `<span class="text">
-        <input type="text" id="todo-text" placeholder="할 일을 입력하세요" autocomplete="off">
-        <button id="add"><span class="lnr lnr-plus-circle"></span></button>
-        </span>
-        `;
-    }
-    //타겟에 따라 발생할 함수
-    if (e.target.closest(".remove")) deleteTodo();
-    else if (e.target.closest(".checkbox input")) {
-        checkTodo() ;
-    }else{
-        return insertTodo();
-    }
-}
-const insertHandler = e => {
-    
-}
-
-// 삭제 이벤트 등록
-$todoUl.addEventListener("click", listHandler);
-
-
-
-
-/*
-4. 할일 체크 기능 - 체크박스를 누르면 put, patch요청으로 db.json에 
-done값을 반대값으로 수정
-
-5. 할 일 수정 기능 - 초록색버튼을 누르면 수정모드로 진입(인풋창 활성화)
- 인풋창에 새로운 텍스트를 쓰고 확인을 누르면 put,patch요청이 나가고
-db.json에서도 수정
-*/
+init();
