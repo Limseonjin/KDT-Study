de466cac438179aecad64f76082908ab1581f43c
SeonJin Lim <hawooks112@gmail.com>
2023-08-10 11:13:17 +0900

    ESLint 설치 및 설정

--- 4.Module/app.js
+++ 4.Module/app.js
@@ -1,11 +1,14 @@
-import {$btn} from './getDOM.js';
-import ABC, {clickHandler as ch} from './event.js';
+// eslint-disable-next-line linebreak-style
+// eslint-disable-next-line linebreak-style
+import { $btn } from './getDOM.js';
+import ABC, { clickHandler as ch } from './event.js';
 
 const init = () => {
-    $btn.style.background = 'aqua';
-    $btn.addEventListener('click',ch);
-    const dog = new ABC('초코',4);
-    dog.info();
+  $btn.style.background = 'aqua';
+  $btn.addEventListener('click', ch);
+  const dog = new ABC('초코', 4);
+  dog.info();
 };
 
+// eslint-disable-next-line eol-last
 init();
