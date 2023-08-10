c04e034d365e994bd10e8b9280f512c49b0ccd0e
SeonJin Lim <hawooks112@gmail.com>
2023-08-10 10:35:09 +0900

    ajax : Module(import,export)연습

---
+++ 4.Module/app.js
@@ -0,0 +1,11 @@
+import {$btn} from './getDOM.js';
+import ABC, {clickHandler as ch} from './event.js';
+
+const init = () => {
+    $btn.style.background = 'aqua';
+    $btn.addEventListener('click',ch);
+    const dog = new ABC('초코',4);
+    dog.info();
+};
+
+init();
---
+++ 4.Module/event.js
@@ -0,0 +1,20 @@
+//getDOM꺼
+import { $p as $para } from './getDOM.js';
+
+//event 꺼 
+const $p = document.querySelector('p');
+
+export const clickHandler = e=>{
+    $para.textContent = '하이!';
+};
+
+//default는 파일에 1개만 ㄱㄴ
+export default class {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    info(){
+        alert(`${this.name}: 저는 ${this.age}살 입니다.`)
+    }
+}
