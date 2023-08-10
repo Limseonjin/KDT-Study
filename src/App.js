4ea4160765b980271fdfe7840d636e932a527d80
SeonJin Lim <hawooks112@gmail.com>
2023-08-10 14:30:08 +0900

    컴포넌트 기초 연습

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import './App.css';
+import AppItem from './AppItem';
+import Noname from './Noname';
+import Hello from './Hello';
 
-function App() {
+const App = () => {
   const $h2 = React.createElement('h2', null, '반가방가~~');
   return (
     <>
-      <div className="App">
-        <h1>메롱메롱</h1>
-        {$h2}
-      </div>
-      <div className="noname">
-        <input type='text'/>
-        <p>
-          오늘은 태풍이 와서 비가 많이 내렸땅. <br />
-          그래서 공부가 하기 실땅
-        </p>
-      </div>
+      <AppItem />
+      <Hello />
+      <Noname />
     </>
   );
-}
+};
 
 export default App;
