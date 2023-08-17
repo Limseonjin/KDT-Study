285ff222708cc9edd8e11d7d9de01f52442e2c79
Seonjin <128454779+Limseonjin@users.noreply.github.com>
2023-08-17 11:35:16 +0900

    동적 클래스 변경 연습

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -27,16 +27,12 @@ const CourseInput = ({ onAdd }) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>나의 목표</label>
         <input
           type="text"
           onChange={textChangeHandler}
           value={enteredText}
-          style={{
-            background: isValid ? 'transparent' : 'salmon',
-            borderColor: isValid ? 'black' : 'red',
-          }}
         />
       </div>
       <Button type="submit">목표 추가하기</Button>
