0ad39ed58bd0eaa400c4ebfcd2267b5602362ff7
Seonjin <128454779+Limseonjin@users.noreply.github.com>
2023-08-18 11:09:12 +0900

    Error 모달 핸들링

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import AddUsers from './components/Users/AddUsers';
 import UserList from './components/Users/UserList';
 
 const App = () => {
- 
+  const [userList, setUserList] = useState([]);
+
+  const addUserHandler = (user) => {
+    setUserList((prev) => [...prev, { ...user, id: Math.random().toString() }]);
+  };
   return (
     <div>
-      <AddUsers />
-      <UserList />
+      <AddUsers onAddUser={addUserHandler} />
+      <UserList users={userList} />
     </div>
   );
 };
