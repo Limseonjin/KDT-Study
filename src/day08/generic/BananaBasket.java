c5c06d1d054578c9088ec6bc7de7b7c0781aaba1
Seonjin <128454779+Limseonjin@users.noreply.github.com>
2023-09-21 12:47:08 +0900

    # generic 연습

---
+++ src/day08/generic/BananaBasket.java
@@ -0,0 +1,13 @@
+package day08.generic;
+
+public class BananaBasket {
+    public Banana getBanana() {
+        return banana;
+    }
+
+    public void setBanana(Banana banana) {
+        this.banana = banana;
+    }
+
+    private Banana banana;
+}
