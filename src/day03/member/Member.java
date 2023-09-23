5be3ecb096b9ae41a4232d2c8592a845d870c15d
Seonjin <128454779+Limseonjin@users.noreply.github.com>
2023-09-23 10:32:57 +0900

    # 텍스트기반 입출력 연습

--- src/day03/member/Member.java
+++ src/day03/member/Member.java
@@ -24,7 +24,7 @@ public class Member {
         this.regDate = LocalDate.now();
     }
 
-    String inform(){
+    public String inform(){
         String convertGender = this.gender == Gender.MALE? "남성" : "여성";
         return String.format("# %d번 | %s | %s | %s | %d세",
                 this.memberId, this.email, this.memberName, convertGender, this.age);
