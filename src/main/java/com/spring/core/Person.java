d0c6300f3619eaa4c67fd0cb1df29716538ee80d
Seonjin <128454779+Limseonjin@users.noreply.github.com>
2023-11-03 11:44:33 +0900

    # 스프링 테스트

---
+++ src/main/java/com/spring/core/Person.java
@@ -0,0 +1,12 @@
+package com.spring.core;
+
+import lombok.*;
+
+@Setter @Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    private String name;
+    private int age;
+}
